92d72650ef2aa4a9ac8cbbed1ed6abe245c463c1
Eric Zhang <zhangzx45@gmail.com>
2025-07-01 09:46:28 -0700

    Add ResponseVm

---
+++ Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿namespace NowPlaying.Models
+{
+    public class Movie
+    {
+        public bool Adult { get; set; }
+        public string? BackdropPath { get; set; }
+        public int[] GenreIds { get; set; } = [];
+        public int Id { get; set; }
+        public string? OriginalLanguage { get; set; }
+        public string? OriginalTitle { get; set; }
+        public string? Overview { get; set; }
+        public float Popularity { get; set; }
+        public string? PosterPath { get; set; }
+        public string? ReleaseDate { get; set; }
+        public string? Title { get; set; }
+        public bool Video { get; set; }
+        public float VoteAverage { get; set; }
+        public int VoteCount { get; set; }
+    }
+
+}
---
+++ Models/MovieListResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace NowPlaying.Models
+{
+    public class MovieListResponse
+    {
+        public int Page { get; set; }
+        public List<Movie> Results { get; set; } = [];
+        public int TotalPages { get; set; }
+        public int TotalResults { get; set; }
+    }
+}
